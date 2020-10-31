e3a118626285b5a9a3198d725b53c41f2c08892e
WineLava <xiaocun@iu.edu>
2020-10-31 18:25:19 -0400

    updates on member record details page

---
+++ app/public/api/ocfr/get-member.php
@@ -0,0 +1,28 @@
+<?php
+
+require 'common.php';
+
+// Step 1: Get a datase connection from our helper class
+$db = DbConnection::getConnection();
+
+// Step 2: Create & run the query
+$sql = 'SELECT * FROM CurrentMembers WHERE member_id = 1';
+$vars = [];
+
+// if (isset($_GET['member_id'])) {
+//   // This is an example of a parameterized query
+//   $sql = 'SELECT * FROM CurrentMembers WHERE member_id = ?';
+//   $vars = [ $_GET['member_id'] ];
+// }
+
+$stmt = $db->prepare($sql);
+$stmt->execute($vars);
+
+$members = $stmt->fetchAll();
+
+// Step 3: Convert to JSON
+$json = json_encode($members, JSON_PRETTY_PRINT);
+
+// Step 4: Output
+header('Content-Type: application/json');
+echo $json;
