d61cba5ba112c35621532676651d8d305e789df9
WineLava <xiaocun@iu.edu>
2020-11-01 02:07:02 -0500

    fixed expired member report, added data validation

--- app/data/schema-patient.sql
+++ app/data/schema-patient.sql
@@ -1,9 +1,9 @@
 USE OCFR;
 
 DROP TABLE IF EXISTS Certified;
-DROP TABLE IF EXISTS CurrentMembers;
-CREATE TABLE CurrentMembers (
-  member_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+DROP TABLE IF EXISTS people;
+CREATE TABLE people (
+  member_id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
   firstname VARCHAR(30) NOT NULL,
   lastname VARCHAR(30) NOT NULL,
   station_number INT Not NULL,
@@ -21,45 +21,25 @@ CREATE TABLE CurrentMembers (
   isActive boolean
 );
 
-INSERT INTO CurrentMembers (firstname, lastname, station_number, radio_number, address_street, address_city, address_state, address_zip, email, date_of_birth, gender, mobile_phone, work_phone, position, isActive) VALUES
+INSERT INTO people (firstname, lastname, station_number, radio_number, address_street, address_city, address_state, address_zip, email, date_of_birth, gender, mobile_phone, work_phone, position, isActive) VALUES
 ( "Samson", "Pollak", "1", "2", "3737 S 7th St", "Terre Haute", "IN", "47802", "spk@iu.edu", "1996-10-12", "Male", "123-234-4657", "435-654-6453", "Fireman", True),
 ( "Maddie", "Lucia", "2", "3", "1010 Indiana Ave", "Bloomington", "IN", "47408", "mal@iu.edu", "1996-08-14", "Female", "321-234-4657", "999-654-6453", "Lawyer", TRUE);
 
--- DROP TABLE IF EXISTS FormerMembers;
--- CREATE TABLE FormerMembers (
---   member_id INT PRIMARY KEY,
---   firstname VARCHAR(30) NOT NULL,
---   lastname VARCHAR(30) NOT NULL,
---   station_number INT,
---   radio_number INT,
---   address_street VARCHAR(50),
---   address_city VARCHAR(50),
---   address_state VARCHAR(50),
---   address_zip VARCHAR(10),
---   email VARCHAR(30),
---   date_of_birth date),
---   gender VARCHAR(1),
---   mobile_phone VARCHAR(10),
---   work_phone VARCHAR(10),
---   position VARCHAR(30),
---   isActive boolean
--- );
-
-INSERT INTO CurrentMembers (firstname, lastname, date_of_birth, gender, isActive) VALUES
+INSERT INTO people (firstname, lastname, date_of_birth, gender, isActive) VALUES
 ("Sylvia", "Hernandez", "2012-09-01",  "F",False),
 ( "Vish", "Balasubramanian", "1950-12-15",  "M",False),
 ( "J", "Doe", "1950-00-00",  "M",False),
 ( "Pepper", "Potts", "1990-01-31",  "F",False);
 
-DROP TABLE IF EXISTS Certification;
-CREATE TABLE Certification (
+DROP TABLE IF EXISTS Certifications;
+CREATE TABLE Certifications (
   certification_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
   certifying_agency VARCHAR(50) NOT NULL,
   certification_name VARCHAR(50) NOT NULL,
   expiration_period int NOT NULL
 );
 
-INSERT INTO Certification (certifying_agency, certification_name, expiration_period) VALUES
+INSERT INTO Certifications (certifying_agency, certification_name, expiration_period) VALUES
 ("AHA", "CPR", "2 years"),
 ("Red Cross", "CPR", "2 years"),
 ("Athens Technical College", "Firefighter I", "3 years"),
@@ -82,9 +62,9 @@ INSERT INTO Certification (certifying_agency, certification_name, expiration_per
 -- ("SOME-DUMMY-DATA", "Pepper", "Potts", "1990-01-31",  "F");
 
 CREATE TABLE Certified (
-  certified_id INT PRIMARY KEY AUTO_INCREMENT,
-  certification_id INT,
-  member_id int,
+  certified_id INT PRIMARY KEY AUTO_INCREMENT not null,
+  certification_id INT not null,
+  member_id int not null,
   issued_date date not null,
   FOREIGN KEY(member_id) REFERENCES CurrentMembers(member_id),
   FOREIGN KEY(certification_id) REFERENCES Certification(certification_id)
