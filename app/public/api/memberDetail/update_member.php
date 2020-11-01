4e3d8c5ae997ba0a284bd7194faa20ae76cc7c5d
WineLava <xiaocun@iu.edu>
2020-11-01 18:57:54 -0500

    added member and certification delete

---
+++ app/public/api/memberDetail/update_member.php
@@ -0,0 +1,61 @@
+<?php
+
+require 'common.php';
+
+// Step 0: Validate the incoming data
+// This code doesn't do that, but should ...
+// For example, if the date is empty or bad, this insert fails.
+
+// Step 1: Get a datase connection from our helper class
+$db = DbConnection::getConnection();
+
+
+$getId = file_get_contents("mid.json");
+// Step 2: Create & run the query
+// Note the use of parameterized statements to avoid injection
+$stmt = $db->prepare(
+  'update CurrentMembers
+  set 
+  firstname = ?,
+  lastname = ?,
+  station_number = ?,
+  radio_number = ?,
+  address_street = ?,
+  address_city = ?,
+  address_state = ?,
+  address_zip = ?,
+  email = ?,
+  date_of_birth = ?,
+  mobile_phone = ?,
+  work_phone = ?,
+  position = ?,
+  isActive = ?
+  where member_id = ?'
+);
+
+$stmt->execute([
+  $_POST['firstname'],
+  $_POST['lastname'],
+  $_POST['station_number'],
+  $_POST['radio_number'],
+  $_POST['address_street'],
+  $_POST['address_city'],
+  $_POST['address_state'],
+  $_POST['address_zip'],
+  $_POST['email'],
+  $_POST['date_of_birth'],
+  $_POST['mobile_phone'],
+  $_POST['work_phone'],
+  $_POST['position'],
+  $_POST['isActive'],
+  [$getId]
+]);
+
+// If needed, get auto-generated PK from DB
+// $pk = $db->lastInsertId();  // https://www.php.net/manual/en/pdo.lastinsertid.php
+
+// Step 4: Output
+// Here, instead of giving output, I'm redirecting to the SELECT API,
+// just in case the data changed by entering it
+header('HTTP/1.1 303 See Other');
+header('Location: /profile.html');
