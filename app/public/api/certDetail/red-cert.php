d91f76408482a4017b0d61473e7e21c1cbb70f0a
malucia1 <70524347+malucia1@users.noreply.github.com>
2020-11-01 20:19:41 -0500

    Updates for Certification Detail PHP

--- app/public/api/certDetail/red-cert.php
+++ app/public/api/certDetail/red-cert.php
@@ -6,7 +6,7 @@ require 'common.php';
 $db = DbConnection::getConnection();
 
 // Step 2: Create & run the query
-$sql = 'SELECT m.firstname, m.lastname, m.email FROM CurrentMembers as m, Certification as c, Certified as cd WHERE m.member_id = cd.member_id AND c.certification_id=cd.certification_id AND c.certification_name="CPR" AND c.certifying_agency="Red Cross"';
+$sql = 'SELECT m.firstname, m.lastname, m.email FROM CurrentMembers as m, Certification as c, Certified as cd WHERE m.member_id = cd.member_id AND c.certification_id=cd.certification_id';
 $vars = [];
 
 // if (isset($_GET['member_id'])) {
